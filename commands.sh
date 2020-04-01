9698c4c3915bb5ef6619b3e450a34abf6d2f2e7f
Aidan Hurley-Kalici <57423704+Aidanjhk@users.noreply.github.com>
2020-04-01 13:11:17 -0400

    t

--- commands.sh
+++ commands.sh
@@ -6,8 +6,8 @@ git add -A
 git commit -m "More Data"
 git push origin master
 
-./Data_Sorting
-
+./Data_Sorting.exe
+./Data_synthesis.exe
 git add -A
 git commit -m "Update Current"
 git push origin master
